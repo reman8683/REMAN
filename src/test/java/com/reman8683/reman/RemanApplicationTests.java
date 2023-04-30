3868a9b15f3fe87692b977412c24f1b2c1291fb0
REMAN <reman8683@gmail.com>
2023-04-30 16:35:13 +0900

    add project

---
+++ src/test/java/com/reman8683/reman/RemanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reman8683.reman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
