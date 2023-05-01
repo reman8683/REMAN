cb1a2b9909b785ab3c18becc2ea98293bbc07b55
REMAN <reman8683@gmail.com>
2023-05-01 14:52:55 +0900

    add project

--- src/main/frontend/src/page/components/Jumbotron.js
+++ src/main/frontend/src/page/components/Jumbotron.js
@@ -1,16 +1,8 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import "../../assets/css/Jumbotron.css";
 import loadingImage from "../../assets/images/loading.png";
 
 export default function Jumbotron(props) {
-    const [thumbnail, setURL] = useState('');
-    useEffect(() => {
-        fetch("/api/randomscreenshot")
-            .then((response) => response.text())
-            .then((data) => {setURL(data);
-            })
-    }, []);
-
     return (
         <div id="jumbotron" style={{
             backgroundImage: `linear-gradient(rgba(0, 0, 0, 1), rgba(0, 0, 0, 0.5)), url("/api/randomscreenshot"), url(${loadingImage})`
